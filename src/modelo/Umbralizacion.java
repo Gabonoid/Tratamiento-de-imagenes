7a1fbd1ad035a7e6479991d5154e4e5396a9af5d
Gabriel Serrano <gabo09link@gmail.com>
2021-09-17 15:58:01 -0500

    Creacion de la funcion umbral en Umbralizacion

--- src/modelo/Umbralizacion.java
+++ src/modelo/Umbralizacion.java
@@ -2,14 +2,29 @@ package modelo;
 
 public class Umbralizacion {
 
-    public short[][] inverso() {
-
-        return null;
+    public static short[][] inverso(short[][] matrizOriginal) {
+        short[][] matrizInverso  = new short [matrizOriginal.length][matrizOriginal[0].length];
+        for (int i = 0; i < matrizOriginal.length; i++) {
+            for (int j = 0; j < matrizOriginal[i].length; j++) {
+                matrizInverso[i][j] = (short) (255 - matrizOriginal[i][j]);   
+            }
+        }
+        return matrizInverso;
     }
-    
-    public short[][] umbral() {
-        
-        return null;
+
+    public static short[][] umbral(short[][] matrizOriginal, short u) {
+        short[][] matrizUmbral  = new short [matrizOriginal.length][matrizOriginal[0].length];
+        u = 253;
+        for (int i = 0; i < matrizOriginal.length; i++) {
+            for (int j = 0; j < matrizOriginal[0].length; j++) {
+                if (matrizOriginal[i][j] <= u){
+                    matrizUmbral[i][j] = 0;
+                } else {
+                    matrizUmbral[i][j] = 255;
+                }
+            }
+        }
+        return matrizUmbral;
     }
 
 }
