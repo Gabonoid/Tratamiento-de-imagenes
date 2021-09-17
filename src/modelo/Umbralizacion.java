15342b26328f81b73227e4fdbffe290006f10df7
Gabriel Serrano <gabo09link@gmail.com>
2021-09-17 16:31:21 -0500

    Creacion de la funcion umbralBinario en Umbralizacion

--- src/modelo/Umbralizacion.java
+++ src/modelo/Umbralizacion.java
@@ -3,21 +3,21 @@ package modelo;
 public class Umbralizacion {
 
     public static short[][] inverso(short[][] matrizOriginal) {
-        short[][] matrizInverso  = new short [matrizOriginal.length][matrizOriginal[0].length];
+        short[][] matrizInverso = new short[matrizOriginal.length][matrizOriginal[0].length];
         for (int i = 0; i < matrizOriginal.length; i++) {
             for (int j = 0; j < matrizOriginal[i].length; j++) {
-                matrizInverso[i][j] = (short) (255 - matrizOriginal[i][j]);   
+                matrizInverso[i][j] = (short) (255 - matrizOriginal[i][j]);
             }
         }
         return matrizInverso;
     }
 
     public static short[][] umbral(short[][] matrizOriginal, short u) {
-        short[][] matrizUmbral  = new short [matrizOriginal.length][matrizOriginal[0].length];
+        short[][] matrizUmbral = new short[matrizOriginal.length][matrizOriginal[0].length];
         u = 253;
         for (int i = 0; i < matrizOriginal.length; i++) {
             for (int j = 0; j < matrizOriginal[0].length; j++) {
-                if (matrizOriginal[i][j] <= u){
+                if (matrizOriginal[i][j] <= u) {
                     matrizUmbral[i][j] = 0;
                 } else {
                     matrizUmbral[i][j] = 255;
@@ -25,6 +25,23 @@ public class Umbralizacion {
             }
         }
         return matrizUmbral;
+    }
+
+    public static short[][] umbralBinario(short[][] matrizOriginal, short u1, short u2) {
+        short[][] matrizUmbralBinario = new short[matrizOriginal.length][matrizOriginal[0].length];
+        u1 = 90;
+        u2 = 180;
+        for (int i = 0; i < matrizOriginal.length; i++) {
+            for (int j = 0; j < matrizOriginal[0].length; j++) {
+                if (matrizOriginal[i][j] < u1 || matrizOriginal[i][j] > u2) {
+                    matrizUmbralBinario[i][j] = 0;
+                }
+                if (u1 > matrizOriginal[i][j] && matrizOriginal[i][j] < u2) {
+                    matrizUmbralBinario[i][j] = 255;
+                }
+            }
+        }
+        return matrizUmbralBinario;
     }
 
 }
